cb705463936bb2c09b17aac6709a2b136ef6a98b
Shvain <takuto3987@gmail.com>
2024-04-13 16:07:35 +0900

    Change X userID & zenn

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './Home.css';
 import { Link } from 'react-router-dom';
-import { X, Instagram} from 'iconoir-react';
+import { X } from 'iconoir-react';
 
 const Home = () => {
   return (
     <div>
-      <Link to="https://twitter.com/lounas_app" target="_blank" className="twitter-link">
+      <Link to="https://twitter.com/iputapp" target="_blank" className="twitter-link">
         <X color='black'  className='x-icon'/>
       </Link>
-      <Link to="https://instagram.com/lounas_app" target="_blank" className="instagram-link">
-        <Instagram color='black'  className='instagram-icon'/>
+      <Link to="https://zenn.dev/p/iput_app" target="_blank" className="zenn-link">
+        <img src={process.env.PUBLIC_URL + '/logo-only-white.svg'} alt='zennlogo'  className='zenn-icon'/>
       </Link>
       <div className='home-container'>
         <video autoPlay loop muted playsInline webkit-playsInline className='background-video'> 
