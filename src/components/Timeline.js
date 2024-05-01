0b4f34b0a3978116a100c68788c703d5f4046110
Shvain <takuto3987@gmail.com>
2024-05-01 23:33:57 +0900

    Add timeline20240427

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -9,6 +9,20 @@ const Timeline = () => {
   return (
   <div>
     <div className='timeline-all'>
+      <div className="day-2024-4-27">
+        <div className='timeline-item'>
+            <div className='date-container'>
+              <span className='date'></span>
+              <p className='day'>2024/4/27</p>
+            </div>
+            <div className='content'>
+              <h2>新入生歓迎会2024にて概要を発表しました。</h2>
+            </div>
+          </div>
+          <div className='image-timeline'>
+            <img src={process.env.PUBLIC_URL + '/iputapp_overviewTop.png'} width='450'  alt='home-page' className='app-home-page'/>
+          </div>  
+      </div>
       <div className="day-2024-3-1">
           <div className='timeline-item'>
               <div className='date-container'>
@@ -22,63 +36,63 @@ const Timeline = () => {
             <div className='image-timeline'>
               <img src={process.env.PUBLIC_URL + '/home-page.png'} width='400'  alt='home-page' className='app-home-page'/>
             </div>  
+      </div>
+      <div className='day-2024-1-4'>
+        <div className='timeline-item'>
+            <div className='date-container'>
+              <span className='date'></span>
+              <p className='day'>2024/1/4</p>
+            </div>
+            <div className='content'>
+              <h2>Project αを発足しました。</h2>
+            </div>
+          </div>
+          <div className='image-timeline'>
+            <img src={process.env.PUBLIC_URL + '/project_a.png'} width='400'  alt='prinlup' className='app-home-page'/>
+          </div>
         </div>
-        <div className='day-2024-1-4'>
-          <div className='timeline-item'>
-              <div className='date-container'>
-                <span className='date'></span>
-                <p className='day'>2024/1/4</p>
-              </div>
-              <div className='content'>
-                <h2>Project αを発足しました。</h2>
-              </div>
+      <div className='day-2023-10-16'>
+        <div className='timeline-item'>
+            <div className='date-container'>
+              <span className='date'></span>
+              <p className='day'>2023/10/16</p>
             </div>
-            <div className='image-timeline'>
-              <img src={process.env.PUBLIC_URL + '/project_a.png'} width='400'  alt='prinlup' className='app-home-page'/>
+            <div className='content'>
+              <h2>lounasのβ版の提供を開始しました。</h2>
             </div>
           </div>
-        <div className='day-2023-10-16'>
-          <div className='timeline-item'>
-              <div className='date-container'>
-                <span className='date'></span>
-                <p className='day'>2023/10/16</p>
-              </div>
-              <div className='content'>
-                <h2>lounasのβ版の提供を開始しました。</h2>
-              </div>
+          <div className='image-timeline'>
+            <img src={process.env.PUBLIC_URL + '/lounas_card.png'} width='250'  alt='prinlup' className='lounas-card'/>
+          </div>
+        </div>
+      <div className='day-2023-9-30'>
+        <div className='timeline-item'>
+            <div className='date-container'>
+              <span className='date'></span>
+              <p className='day'>2023/9/30</p>
             </div>
-            <div className='image-timeline'>
-              <img src={process.env.PUBLIC_URL + '/lounas_card.png'} width='250'  alt='prinlup' className='lounas-card'/>
+            <div className='content'>
+              <h2>IPUT FESTA2023にてlounasを発表しました。</h2>
             </div>
           </div>
-        <div className='day-2023-9-30'>
-          <div className='timeline-item'>
-              <div className='date-container'>
-                <span className='date'></span>
-                <p className='day'>2023/9/30</p>
-              </div>
-              <div className='content'>
-                <h2>IPUT FESTA2023にてlounasを発表しました。</h2>
-              </div>
+          <div className='image-timeline'>
+            <img src={process.env.PUBLIC_URL + '/Release-teaser.png'} width='500'  alt='prinlup' className='lounas-presents'/>
+          </div>
+        </div>
+      <div className='day-2023-7-1'>
+        <div className='timeline-item'>
+            <div className='date-container'>
+              <span className='date'></span>
+              <p className='day'>2023/6/9</p>
             </div>
-            <div className='image-timeline'>
-              <img src={process.env.PUBLIC_URL + '/Release-teaser.png'} width='500'  alt='prinlup' className='lounas-presents'/>
+            <div className='content'>
+              <h2>アプリ開発サークルを設立しました。</h2>
             </div>
           </div>
-        <div className='day-2023-7-1'>
-          <div className='timeline-item'>
-              <div className='date-container'>
-                <span className='date'></span>
-                <p className='day'>2023/7/1</p>
-              </div>
-              <div className='content'>
-                <h2>アプリ開発サークルを設立しました。</h2>
-              </div>
-            </div>
-            <div className='image-timeline'>
-              <img src={process.env.PUBLIC_URL + '/app_dev_logo.png'} width='250'  alt='prinlup' className='app-dev'/>
-            </div>
+          <div className='image-timeline'>
+            <img src={process.env.PUBLIC_URL + '/app_dev_logo.png'} width='250'  alt='prinlup' className='app-dev'/>
           </div>
+        </div>
       </div>
   </div>
   );
